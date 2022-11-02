ea591baf3c53e48705227426f57c04474e967394
mlmcoretech <lordvorkkloc@gmail.com>
2022-11-02 04:17:38 -0500

    cool

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -38,25 +38,8 @@ class HomeController extends Controller
     public function rates()
     {
         
-
-        // return Cache::remember('city' . $city, 60 * 5, function() use ($coordinates) {
-            $rates = Http::get('http://vorkkloc.localhost/api/rates');
-            
-          
-        //     if ($rates->successful()) {
-        //         return rates;
-        //     }
-
-        //     return rates=[];
-        // });
-
-        // $rates=$rates=$rates->json()   ;
-        $rates=$rates=json_decode($rates)   ;
-        
-        $links =$rates->meta->links;
-
-        //  dd($rates->meta->links);   ;
-        return view('pages.rates.rates',  compact('rates','links'));
+ 
+        return view('pages.rates.rates' );
     }
 
 
