df69b4108ebca34b51ab9121a0728f611783848a
MLmcore <mlmartin89@yahoo.com>
2025-03-30 22:45:18 -0500

    Fix indentation in rates page layout for improved readability

--- resources/views/pages/rates/rates.blade.php
+++ resources/views/pages/rates/rates.blade.php
@@ -15,7 +15,7 @@
         <div class="    max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="  overflow-hidden shadow-sm sm:rounded-lg">
 
-              <div class=" pt-16 mb-8 "> 
+                 <div class=" pt-16 mb-8 "> 
                     <div class="grid grid-cols-1 md:grid-cols-2 gap-6 mb-8 w-full">
                      
                         <div class="bg-white p-6 rounded-lg shadow-md">
