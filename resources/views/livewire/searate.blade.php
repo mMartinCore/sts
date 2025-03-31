120e92e91ccae3650099f084fc9923b6249b511b
MLmcore <mlmartin89@yahoo.com>
2025-03-31 12:54:12 -0500

    Update volumetric pricing note for clarity in separate page layout

--- resources/views/livewire/searate.blade.php
+++ resources/views/livewire/searate.blade.php
@@ -74,7 +74,7 @@
                     <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"/>
                 </svg>
                 <p class="text-sm font-medium leading-tight">
-                    Packages over 100 lbs use volumetric pricing: (L × W × H in inches) ÷ 166
+                    Packages over 100 lbs use volumetric pricing: (L × W × H in inches)  
                 </p>
             </div>
         </div>
